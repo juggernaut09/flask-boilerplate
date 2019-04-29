b6fbd85fda24713b269f8aaf83cc98261dd868df
Venkat teja Raavi <venkatteja.ravi@gmail.com>
2019-04-29 12:29:34 +0530

    Add files via upload

---
+++ flask-boilerplate/dbo/client.py
@@ -0,0 +1,5 @@
+from pymongo import MongoClient
+from app import app
+from instance.dev import MONGODB
+client = MongoClient(app.config['MONGODB']['url'], app.config['MONGODB']['port'])
+db = client[app.config['MONGODB']['database']]
---
+++ flask-boilerplate/instance/dev.py
@@ -0,0 +1,6 @@
+MONGODB={
+    'url':'localhost',
+    'url':'127.0.0.1',
+    'port':27017,
+    'database':'chitraka-dev'
+}
